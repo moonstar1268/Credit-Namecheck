7c0c340c6a1d27015cebd7e62e7a7ef4672ff9b6
moonstar1268 <72325564+moonstar1268@users.noreply.github.com>
2025-06-22 21:13:50 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,6 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
 
+app.get('/', (req, res) => res.send('Hello Backend!'));
+app.listen(4000, () => console.log('서버가 4000 포트에서 작동중입니다.'));
